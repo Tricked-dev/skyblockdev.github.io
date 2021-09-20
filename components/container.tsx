3c081cac329a858fd693a9ccbdcae8b65b0fc5c7
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-09-20 21:12:36 +0200

    make website a pwa

--- components/container.tsx
+++ components/container.tsx
@@ -15,10 +15,12 @@ export default function Container(props: any) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: "Tricked",
+    title: "Tricked.pro",
     description: `Typescript/javscript developer`,
-    // image: "https://tricked.pro/",
     type: "website",
+    themeColor: "#317EFB",
+    keywords: "javascript typescript programming discord matrix",
+    viewport: "width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no",
     ...customMeta,
   };
 
@@ -26,20 +28,53 @@ export default function Container(props: any) {
     <div>
       <Head>
         <title>{meta.title}</title>
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/icons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="msapplication-TileColor" content={meta.themeColor} />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/assets/icons/icon-48x48.png" rel="icon" type="image/png" sizes="48x48" />
+        <link href="/assets/icons/icon-72x72.png" rel="icon" type="image/png" sizes="72x72" />
+        <link rel="apple-touch-icon" href="/assets/icons/icon-512x512.png"></link>
+        <link rel="canonical" href={`https://tricked.pro${router.asPath}`} />
+
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content={meta.viewport} />
+        <meta name="keywords" content={meta.keywords} />
+        <meta name="theme-color" content={meta.themeColor} />
+
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
+
         <meta property="og:url" content={`https://tricked.pro${router.asPath}`} />
-        <link rel="canonical" href={`https://tricked.pro${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Tricked" />
+        <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tricked" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
+
         {meta.date && <meta property="article:published_time" content={meta.date} />}
       </Head>
       <Nav />
--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
 });
-module.exports = withMDX({
-  reactStrictMode: true,
-  compress: false,
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    loader: "imgix",
-    path: "",
-  },
-  target: "serverless",
-});
+const withPWA = require("next-pwa");
+module.exports = withPWA(
+  withMDX({
+    pwa: {
+      dest: "public",
+    },
+    reactStrictMode: true,
+    compress: false,
+    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      loader: "imgix",
+      path: "",
+    },
+    target: "serverless",
+  })
+);
