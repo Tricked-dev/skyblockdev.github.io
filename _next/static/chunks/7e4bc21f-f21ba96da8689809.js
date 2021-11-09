16250975f5063958b3a081e17e5427c78074310f
Tricked-dev <Tricked-dev@users.noreply.github.com>
2021-11-09 19:57:47 +0000

    deploy: c73dc28c8145d476e3c0236d1fdfae60b0751ffe

---
+++ _next/static/chunks/7e4bc21f-f21ba96da8689809.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[291],{55027:function(t,a,n){var e=n(88357).w_;t.exports.D6S=function(t){return e({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"}}]})(t)}}}]);
